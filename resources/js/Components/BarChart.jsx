6a21c3341e702f7d51de814346b35e56b7a7e118
IAMR7 <reza.febri72@gmail.com>
2024-01-13 20:36:29 +0700

    tweak padding barchart

--- resources/js/Components/BarChart.jsx
+++ resources/js/Components/BarChart.jsx
@@ -1,8 +1,8 @@
 import { Bar } from "react-chartjs-2";
 export const BarChart = ({ chartData }) => {
     return (
-      <div className="chart-container">
-        <h2 style={{ textAlign: "center" }}>Bar Chart</h2>
+      <div className="chart-container p-10">
+        <h2 className="font-semibold text-lg text-center">Judul Bar-chart</h2>
         <Bar
           data={chartData}
           options={{
